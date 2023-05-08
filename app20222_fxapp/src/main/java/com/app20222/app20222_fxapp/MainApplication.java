f0bfb637546762b93f69e542f10c688a096dd1be
Chien Dao <chiendao1808@gmail.com>
2023-05-08 16:28:38 +0700

    APP_20222 : Refract fxapp source code

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/MainApplication.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/MainApplication.java
@@ -10,13 +10,15 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
 
+    public static final String DEFAULT_RESOURCE_PATH = "com/app20222/app20222_fxapp";
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/login_view/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        Image icon = new Image(MainApplication.class.getResource("views/Image/logo.png").openStream());
+        Image icon = new Image(MainApplication.class.getResource( "views/login_view/img/logo.png").openStream());
         stage.getIcons().add(icon);
-        stage.setTitle("Login");
+        stage.setTitle("Hệ thống quản lý phẫu thuật");
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.show();
