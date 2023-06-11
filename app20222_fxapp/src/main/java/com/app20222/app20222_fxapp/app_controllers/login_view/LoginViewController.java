6b8b7ab02c1f68ed791b6fe676504ccf8f2895fb
Chien Dao <chiendao1808@gmail.com>
2023-06-12 00:20:34 +0700

    APP_20222: Fix identityType, clear token when logout and reformat fx code

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/login_view/LoginViewController.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/login_view/LoginViewController.java
@@ -25,6 +25,7 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class LoginViewController {
+
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -49,15 +50,15 @@ public class LoginViewController {
      */
     public void switchToSceneMain(ActionEvent event) throws IOException {
         Alert alert;
-        if(username.getText().isEmpty() || password.getText().isEmpty()){
+        if (username.getText().isEmpty() || password.getText().isEmpty()) {
             alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Thông báo");
             alert.setHeaderText(null);
             alert.setContentText("Vui lòng nhập đầy đủ tài khoản và mật khẩu!");
             alert.showAndWait();
         } else {
-            Integer check =  login(username.getText(), password.getText());
-            if(Objects.equals(check, HttpStatusCodes.STATUS_CODE_OK)) {
+            Integer check = login(username.getText(), password.getText());
+            if (Objects.equals(check, HttpStatusCodes.STATUS_CODE_OK)) {
                 alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Thông báo");
                 alert.setHeaderText(null);
@@ -66,7 +67,7 @@ public class LoginViewController {
                 //   Ẩn màn login
                 loginBtn.getScene().getWindow().hide();
                 root = FXMLLoader.load(Objects.requireNonNull(MainApplication.class.getResource("views/main_view/main-view.fxml")));
-                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 scene = new Scene(root);
                 stage.setTitle("Hospital-Systems");
                 stage.setScene(scene);
@@ -90,42 +91,43 @@ public class LoginViewController {
      */
     @FXML
     public void togglePasswordVisibility(ActionEvent event) {
-            if (passwordVisibilityCheckbox.isSelected()) {
-                password.setVisible(false);
-                plainPassword.setVisible(true);
-                plainPassword.setText(password.getText());
-            } else {
-                password.setVisible(true);
-                plainPassword.setVisible(false);
-                password.setText(plainPassword.getText());
-            }
+        if (passwordVisibilityCheckbox.isSelected()) {
+            password.setVisible(false);
+            plainPassword.setVisible(true);
+            plainPassword.setText(password.getText());
+        } else {
+            password.setVisible(true);
+            plainPassword.setVisible(false);
+            password.setText(plainPassword.getText());
+        }
 
     }
 
     /**
      * Hàm đăng nhập -> lấy token
      */
-    private Integer login(String username, String password){
-      try{
-          String apiPath = APIDetails.AUTH_LOGIN.getRequestPath() + APIDetails.AUTH_LOGIN.getDetailPath();
-          String uri = ApiUtils.buildURI(apiPath, new HashMap<>());
-          HttpResponse response = HttpUtils.doRequest(uri, HttpMethods.POST, new LoginRequest(username, password), new HashMap<>());
-          // Check login response
-          if(Objects.equals(response.statusCode(), HttpStatusCodes.STATUS_CODE_OK)
-                  || Objects.equals(response.statusCode(), HttpStatusCodes.STATUS_CODE_ACCEPTED)){
-              LoginResponse loginResponse = HttpUtils.mappingResponseBody(response, new TypeReference<LoginResponse>(){});
-              // Set authentication info in application context
-              ApplicationContext.ACCESS_TOKEN = loginResponse.getAccessToken();
-              ApplicationContext.REFRESH_TOKEN = loginResponse.getRefreshToken();
-              ApplicationContext.roles = loginResponse.getRoles();
-              System.out.println(ApplicationContext.roles);
-              return response.statusCode();
-          }
-      }catch (Exception exception){
-          exception.printStackTrace();
-          return -1;
-      }
-      return -1;
+    private Integer login(String username, String password) {
+        try {
+            String apiPath = APIDetails.AUTH_LOGIN.getRequestPath() + APIDetails.AUTH_LOGIN.getDetailPath();
+            String uri = ApiUtils.buildURI(apiPath, new HashMap<>());
+            HttpResponse response = HttpUtils.doRequest(uri, HttpMethods.POST, new LoginRequest(username, password), new HashMap<>());
+            // Check login response
+            if (Objects.equals(response.statusCode(), HttpStatusCodes.STATUS_CODE_OK)
+                || Objects.equals(response.statusCode(), HttpStatusCodes.STATUS_CODE_ACCEPTED)) {
+                LoginResponse loginResponse = HttpUtils.mappingResponseBody(response, new TypeReference<LoginResponse>() {
+                });
+                // Set authentication info in application context
+                ApplicationContext.ACCESS_TOKEN = loginResponse.getAccessToken();
+                ApplicationContext.REFRESH_TOKEN = loginResponse.getRefreshToken();
+                ApplicationContext.roles = loginResponse.getRoles();
+                System.out.println(ApplicationContext.roles);
+                return response.statusCode();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return -1;
+        }
+        return -1;
     }
 
 }
