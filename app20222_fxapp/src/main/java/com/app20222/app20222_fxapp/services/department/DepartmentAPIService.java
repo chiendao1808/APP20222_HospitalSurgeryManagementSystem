2b2f3f021611bf327fdac734cba5ddfdca76d1b3
Chien Dao <chiendao1808@gmail.com>
2023-08-01 00:10:07 +0700

    APP_20222: Add APIService for medicalRecord, department and surgeryRoom

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/services/department/DepartmentAPIService.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/services/department/DepartmentAPIService.java
@@ -1,5 +1,68 @@
 package com.app20222.app20222_fxapp.services.department;
 
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.app20222.app20222_fxapp.dto.requests.department.DepartmentCreateDTO;
+import com.app20222.app20222_fxapp.dto.requests.department.DepartmentUpdateDTO;
+import com.app20222.app20222_fxapp.dto.responses.deparment.DepartmentListDTO;
+import com.app20222.app20222_fxapp.dto.responses.exception.ExceptionResponse;
+import com.app20222.app20222_fxapp.enums.apis.APIDetails;
+import com.app20222.app20222_fxapp.exceptions.apiException.ApiResponseException;
+import com.app20222.app20222_fxapp.utils.apiUtils.ApiUtils;
+import com.app20222.app20222_fxapp.utils.httpUtils.HttpUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 public class DepartmentAPIService {
+
+    /**
+     * API Call getListDepartment
+     */
+    public List<DepartmentListDTO> getListDepartment(Map<String, String> params) throws ApiResponseException {
+        List<DepartmentListDTO> departments;
+        String uri = ApiUtils.buildURI(APIDetails.DEPARTMENT_GET_LIST.getRequestPath() + APIDetails.DEPARTMENT_GET_LIST.getDetailPath(), params);
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.DEPARTMENT_GET_LIST.getMethod(), null, params);
+        // api call successfully (status = 200)
+        if (HttpUtils.isCallSuccessfully(response)) {
+            departments = HttpUtils.handleResponse(response, new TypeReference<>() {});
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return departments;
+    }
+
+    /**
+     * API call createDepartment
+     */
+    public Boolean createDepartment(DepartmentCreateDTO createDTO) {
+        String uri = ApiUtils.buildURI(APIDetails.DEPARTMENT_CREATE.getRequestPath() + APIDetails.DEPARTMENT_CREATE.getDetailPath(), new HashMap<>());
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.DEPARTMENT_CREATE.getMethod(), createDTO, new HashMap<>());
+        Boolean isSuccess = HttpUtils.isCallSuccessfully(response);
+        if (isSuccess) {
+            System.out.println("response: Created successfully");
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return isSuccess;
+    }
+
+    /**
+     * API call updateDepartment
+     */
+    public Boolean updateDepartment(DepartmentUpdateDTO updateDTO) {
+        String uri = ApiUtils.buildURI(APIDetails.DEPARTMENT_UPDATE.getRequestPath() + APIDetails.DEPARTMENT_UPDATE.getDetailPath(), new HashMap<>());
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.DEPARTMENT_UPDATE.getMethod(), updateDTO, new HashMap<>());
+        Boolean isSuccess = HttpUtils.isCallSuccessfully(response);
+        if (isSuccess) {
+            System.out.println("response: Updated successfully");
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return isSuccess;
+    }
 
 }
--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/services/surgeryRoom/SurgeryRoomAPIService.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/services/surgeryRoom/SurgeryRoomAPIService.java
@@ -1,5 +1,68 @@
 package com.app20222.app20222_fxapp.services.surgeryRoom;
 
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.app20222.app20222_fxapp.dto.requests.surgeryRoom.SurgeryRoomCreateDTO;
+import com.app20222.app20222_fxapp.dto.requests.surgeryRoom.SurgeryRoomUpdateDTO;
+import com.app20222.app20222_fxapp.dto.responses.exception.ExceptionResponse;
+import com.app20222.app20222_fxapp.dto.responses.surgeryRoom.SurgeryRoomListDTO;
+import com.app20222.app20222_fxapp.enums.apis.APIDetails;
+import com.app20222.app20222_fxapp.exceptions.apiException.ApiResponseException;
+import com.app20222.app20222_fxapp.utils.apiUtils.ApiUtils;
+import com.app20222.app20222_fxapp.utils.httpUtils.HttpUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 public class SurgeryRoomAPIService {
+
+    /**
+     * API Call getListSurgeryRoom
+     */
+    public List<SurgeryRoomListDTO> getListSurgeryRoom(Map<String, String> params) throws ApiResponseException {
+        List<SurgeryRoomListDTO> surgeryRooms;
+        String uri = ApiUtils.buildURI(APIDetails.SURGERY_ROOM_GET_LIST.getRequestPath() + APIDetails.SURGERY_ROOM_GET_LIST.getDetailPath(), params);
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.SURGERY_ROOM_GET_LIST.getMethod(), null, params);
+        // api call successfully (status = 200)
+        if (HttpUtils.isCallSuccessfully(response)) {
+            surgeryRooms = HttpUtils.handleResponse(response, new TypeReference<>() {});
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return surgeryRooms;
+    }
+
+    /**
+     * API call createSurgeryRoom
+     */
+    public Boolean createSurgeryRoom(SurgeryRoomCreateDTO createDTO) {
+        String uri = ApiUtils.buildURI(APIDetails.SURGERY_ROOM_CREATE.getRequestPath() + APIDetails.SURGERY_ROOM_CREATE.getDetailPath(), new HashMap<>());
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.SURGERY_ROOM_CREATE.getMethod(), createDTO, new HashMap<>());
+        Boolean isSuccess = HttpUtils.isCallSuccessfully(response);
+        if (isSuccess) {
+            System.out.println("response: Created successfully");
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return isSuccess;
+    }
+
+    /**
+     * API call updateSurgeryRoom
+     */
+    public Boolean updateSurgeryRoom(SurgeryRoomUpdateDTO updateDTO) {
+        String uri = ApiUtils.buildURI(APIDetails.SURGERY_ROOM_UPDATE.getRequestPath() + APIDetails.SURGERY_ROOM_UPDATE.getDetailPath(), new HashMap<>());
+        HttpResponse<String> response = HttpUtils.doRequest(uri, APIDetails.SURGERY_ROOM_UPDATE.getMethod(), updateDTO, new HashMap<>());
+        Boolean isSuccess = HttpUtils.isCallSuccessfully(response);
+        if (isSuccess) {
+            System.out.println("response: Updated successfully");
+        } else {
+            ExceptionResponse exceptionResponse = HttpUtils.handleResponse(response, new TypeReference<>() {});
+            throw new ApiResponseException(exceptionResponse);
+        }
+        return isSuccess;
+    }
 
 }
