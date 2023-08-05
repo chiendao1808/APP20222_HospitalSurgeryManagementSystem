ed9007390dba4b35a50e93a5e1ac3a0a16c632b3
tuanduong <phanduong2510@gmail.com>
2023-08-05 17:29:07 +0700

    click button action to get data

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/user_view/UserController.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/user_view/UserController.java
@@ -1,9 +1,15 @@
 package com.app20222.app20222_fxapp.app_controllers.user_view;
 
 import com.app20222.app20222_fxapp.MainApplication;
+import com.app20222.app20222_fxapp.app_controllers.patient_view.AddPatientController;
+import com.app20222.app20222_fxapp.dto.responses.patient.PatientDetailsDTO;
+import com.app20222.app20222_fxapp.dto.responses.patient.PatientGetListNewDTO;
+import com.app20222.app20222_fxapp.dto.responses.users.UserDetailsDTO;
 import com.app20222.app20222_fxapp.dto.responses.users.UserListDTO;
 import com.app20222.app20222_fxapp.exceptions.apiException.ApiResponseException;
 import com.app20222.app20222_fxapp.services.users.UserAPIService;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,12 +18,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotResult;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -61,6 +71,7 @@ public class UserController {
     private Button createUserBtn;
 
     private UserAPIService userAPIService;
+    Map<String,String> map = new HashMap<>();
 
     public UserController(){}
 
@@ -91,36 +102,44 @@ public class UserController {
         // Lấy danh sách bệnh nhân từ nguồn dữ liệu của bạn
         ObservableList<UserListDTO> userList = getDataFromDataSource();
         setupTableColumns();
-//        setupEditDeleteButtons();
+        setUpEditDeleteButton();
 
         if (UserTableView != null) {
             this.UserTableView.setItems(userList);
         }
     }
 
-    public void openCreateDialog() {
-        String fxmlPath = "views/user_view/create.fxml";
-        try {
-            FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
-            Parent root = loader.load();
+//    public void openEditDialog(UserDetailsDTO userDetailsDTO , Map<String,String> param) {
+//        String fxmlPath = "views/user_view/create.fxml";
+//        try {
+//            FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
+//            Parent root = loader.load();
 //            AddUserController addUserController = loader.getController();
-//            addUserController.setCreateMode(true);
-//            addUserController.initialize(null, null);
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Tạo mới người dùng");
-            dialogStage.setScene(new Scene(root));
-            dialogStage.setOnHidden(e -> {
-                Boolean resultSubmit = true;
-                if (Objects.equals(resultSubmit, true)) {
-                    reloadTable();
-                }
-            });
-            dialogStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//            addUserController.disableAllFields();
+//            Stage dialogStage = new Stage();
+//            dialogStage.setTitle("Xem chi tiết người dùng");
+//            dialogStage.setScene(new Scene(root));
+//            addUserController.setUserId(param);
+//            addUserController.setText(
+//                    userDetailsDTO.getIdentificationNum(),
+//                    userDetailsDTO.getIdentityType(),
+//                    userDetailsDTO.getPhoneNumber(),
+//                    userDetailsDTO.getBirthDate(),
+//                    userDetailsDTO.getAddress(),
+//                    userDetailsDTO.getDepartment(),
+//                    userDetailsDTO.getRole(),
+//                    userDetailsDTO.getEmail(),
+//
+//
+//
+//
+//
+//            );
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
     public void setupTableColumns() {
         this.UserSttColumn.setCellValueFactory(param -> {
             int index = param.getTableView().getItems().indexOf(param.getValue());
@@ -139,6 +158,88 @@ public class UserController {
         ObservableList<UserListDTO> lstUsers = getDataFromDataSource();
         this.UserTableView.setItems(lstUsers);
     }
+    public void setUpEditDeleteButton() {
+        Callback<TableColumn<UserListDTO, String>, TableCell<UserListDTO, String>> cellCallback = new Callback<TableColumn<UserListDTO, String>, TableCell<UserListDTO, String>>() {
+            @Override
+            public TableCell<UserListDTO, String> call(TableColumn<UserListDTO, String> userListDTOStringTableColumn) {
+                final TableCell<UserListDTO, String> cell = new TableCell<>() {
+                    private final Button viewButton = new Button();
+
+                    {
+                        FontAwesomeIconView viewIcon = new FontAwesomeIconView(FontAwesomeIcon.EYE);
+                        viewIcon.setStyle(" -fx-cursor: hand ;"
+                                + "-glyph-size:24px;"
+                                + "-fx-fill:#00E676;");
+
+                        viewButton.setGraphic(viewIcon);
+                        viewButton.getStyleClass().add("edit-button");
+                        viewButton.setOnAction(actionEvent -> {
+                            UserListDTO userDTO = getTableView().getItems().get(getIndex());
+                            System.out.println("user" + userDTO);
+                            Map<String, String> map = new HashMap<>();
+                            map.put("id", String.valueOf(userDTO.getId()));
+                            try {
+                                UserDetailsDTO userDetail = userAPIService.getDetailsUser(map);
+                                System.out.println("userDetail " + userDetail);
+
+                            } catch (ApiResponseException exception) {
+                                System.out.println(exception.getExceptionResponse());
+                            }
+                        });
+                        setGraphic(viewButton);
+                    }
+                    @Override
+                    protected void updateItem(String item , boolean empty){
+                        super.updateItem(item,empty);
+                        if(empty){
+                            setGraphic(null);
+                        }
+                        else {
+                            setGraphic(viewButton);
+                        }
+                    }
+                };
+                return cell ;
+            }
+        };
+        UserActionColumn.setCellFactory(cellCallback);
+    }
+
+//    public void openEditDialog(UserDetailsDTO userDetailsDTO , Map<String,String> map){
+//        String fxmlPath =  "views/user_view/create.fxml";
+//        try {
+//            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
+//            Parent root = fxmlLoader.load();
+//            AddUserController addUserController = fxmlLoader.getController();
+//            addUserController.disi
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+public void openCreateDialog() {
+    String fxmlPath = "views/user_view/create.fxml";
+    try {
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(fxmlPath));
+        Parent root = loader.load();
+        AddUserController addUserController = loader.getController();
+        addUserController.setCreateMode(true);
+//        addUserController.initialize(null, null);
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Tạo mới người dùng");
+        dialogStage.setScene(new Scene(root));
+        dialogStage.setOnHidden(e -> {
+            Boolean resultSubmit = addUserController.submit();
+            if (Objects.equals(resultSubmit, true)) {
+                reloadTable();
+            }
+        });
+        dialogStage.show();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 
 
     // Show dialogStage thêm mới người dùng
@@ -158,6 +259,7 @@ public class UserController {
             exception.printStackTrace();
             System.out.println(exception.getExceptionResponse());
         }
+        
         return FXCollections.observableArrayList(lstUsers);
     }
 }
