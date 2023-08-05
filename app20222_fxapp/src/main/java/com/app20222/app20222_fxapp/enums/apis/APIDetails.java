e1ecc36791aaa8bd9e872f64d72f9c7889cd880f
truongnv <truongnv@vnpt-technology.vn>
2023-08-05 20:20:26 +0700

    APP_20222: Thêm tạo cho hồ sơ bệnh án, clear param khi chuyển màn

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/enums/apis/APIDetails.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/enums/apis/APIDetails.java
@@ -51,6 +51,8 @@ public enum APIDetails {
     MEDICAL_RECORD_GET_DETAILS("MEDICAL_RECORD_GET_DETAIL","/medical-record", "/get-detail", "GET"),
     MEDICAL_RECORD_UPDATE("MEDICAL_RECORD_UPDATE", "/medical-record", "/", "PUT"),
 
+    // File
+    FILE_UPLOAD("FILE_UPLOAD", "/file-attach", "/upload-doc", "POST"),
     // ComboBox
     GET_COMBO_BOX_USER("GET_COMBO_BOX_USER","/combo-box", "/users/get-lst-user-name-code", "GET"),
     GET_COMBO_BOX_ROLE("GET_COMBO_BOX_ROLE","/combo-box", "/role/get-lst-role-name-code", "GET"),
