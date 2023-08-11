3184edf3ee610fbcc9613fa508540e9107eac9f7
tuanduong <phanduong2510@gmail.com>
2023-08-11 20:23:53 +0700

    fixx

--- app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/user_view/AddUserController.java
+++ app20222_fxapp/src/main/java/com/app20222/app20222_fxapp/app_controllers/user_view/AddUserController.java
@@ -1,16 +1,19 @@
 package com.app20222.app20222_fxapp.app_controllers.user_view;
 
 import com.app20222.app20222_fxapp.dto.common.CommonIdCodeName;
+import com.app20222.app20222_fxapp.dto.requests.users.UserCreateDTO;
 import com.app20222.app20222_fxapp.enums.users.IdentityTypeEnum;
 import com.app20222.app20222_fxapp.enums.users.RoleEnum;
 import com.app20222.app20222_fxapp.exceptions.apiException.ApiResponseException;
 import com.app20222.app20222_fxapp.services.comboBox.ComboBoxAPIService;
 import com.app20222.app20222_fxapp.services.users.UserAPIService;
+import com.app20222.app20222_fxapp.utils.DateUtils;
 import com.app20222.app20222_fxapp.utils.Validation;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
 import org.controlsfx.control.CheckComboBox;
@@ -23,13 +26,16 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class AddUserController {
+import static com.app20222.app20222_fxapp.utils.Validation.isValidEmail;
+import static com.app20222.app20222_fxapp.utils.Validation.isValidPhoneNumber;
+
+public class AddUserController implements Initializable {
 
     @FXML
     private TextField CodeNumberView;
 
     @FXML
-    private ComboBox<CommonIdCodeName> DepartmentIdView;
+    private ComboBox<String> DepartmentIdView;
 
     @FXML
     private CheckComboBox<String> RoleListIdView;
@@ -69,7 +75,7 @@ public class AddUserController {
     private Boolean reloadRequired = false;
     private final Map<String,String> identityTypeMap = new HashMap<>();
     private final Map<String,String> roleTypeMap = new HashMap<>();
-    private ComboBoxAPIService comboBoxAPIService  ;
+    private ComboBoxAPIService comboBoxAPIService = new ComboBoxAPIService();
 
 
     @FXML
@@ -171,44 +177,55 @@ public Boolean submit() {
         Button cancelButton = (Button) createUserPane.lookupButton(ButtonType.CANCEL);
         cancelButton.setOnAction(event -> handleButtonAction(ButtonType.CANCEL));
     }
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
         setupIdentityTypes();
         setBirthDateView();
         setUpDepartment();
         setupRoleType();
         setupButtonEventFilters();
-        comboBoxAPIService = new ComboBoxAPIService();
         userAPIService = new UserAPIService();
-        if (createMode) {
-            editUser.setVisible(false); // Hide the button for create mode
+
+             // Hide the button for create mode
             Button okButton = (Button) createUserPane.lookupButton(ButtonType.OK);
             okButton.setVisible(true);
             Button cancelButton = (Button) createUserPane.lookupButton(ButtonType.CANCEL);
             cancelButton.setVisible(true);
-        } else {
-            editUser.setVisible(true); // Show the button for view mode
-            Button okButton = (Button) createUserPane.lookupButton(ButtonType.OK);
-            okButton.setVisible(false);
-            Button cancelButton = (Button) createUserPane.lookupButton(ButtonType.CANCEL);
-            cancelButton.setVisible(false);
-        }
     }
     public void setUpDepartment(){
-        ObservableList<CommonIdCodeName> listPatientId = getDataFromDataDepartmentSource();
-        ObservableList<CommonIdCodeName> observableList = FXCollections.observableArrayList(listPatientId);
-        DepartmentIdView.setConverter(new StringConverter<CommonIdCodeName>() {
+//        ObservableList<CommonIdCodeName> listPatientId = getDataFromDataDepartmentSource();
+//        ObservableList<CommonIdCodeName> observableList = FXCollections.observableArrayList(listPatientId);
+//        DepartmentIdView.setConverter(new StringConverter<>() {
+//            @Override
+//            public String toString(CommonIdCodeName item) {
+//                return item.getName() ; // Hiển thị tên bệnh nhân trong ComboBox
+//            }
+//
+//            @Override
+//            public CommonIdCodeName fromString(String string) {
+//                // Chuyển đổi ngược từ chuỗi (nếu cần)
+//                return null;
+//            }
+//        });
+//        DepartmentIdView.setItems(observableList);
+        ObservableList<CommonIdCodeName> listDepartment = getDataFromDataDepartmentSource();
+        System.out.println("List department " + listDepartment);
+        ObservableList<String> names = FXCollections.observableArrayList();
+        for(CommonIdCodeName cic : listDepartment){
+            names.add(cic.getName());
+        }
+        DepartmentIdView.setConverter(new StringConverter<String>() {
             @Override
-            public String toString(CommonIdCodeName item) {
-                return item.getName() + " - " + item.getCode(); // Hiển thị tên bệnh nhân trong ComboBox
+            public String toString(String item) {
+                return item ;
             }
 
             @Override
-            public CommonIdCodeName fromString(String string) {
-                // Chuyển đổi ngược từ chuỗi (nếu cần)
+            public String fromString(String s) {
                 return null;
             }
         });
-        DepartmentIdView.setItems(observableList);
+        DepartmentIdView.setItems(names);
     }
     public void setupRoleType(){
         roleTypeMap.put(RoleEnum.DEPARTMENT_ADMIN.name() ,RoleEnum.DEPARTMENT_ADMIN.getRoleName());
@@ -221,14 +238,13 @@ public Boolean submit() {
         roleTypeMap.put(RoleEnum.STAFF.name(), RoleEnum.STAFF.getRoleName());
         // Create an ObservableList to hold the labels for the identity types
         ObservableList<String> roleTypeLabels = FXCollections.observableArrayList();
-        for (String s : roleTypeMap.values()){
-            roleTypeLabels.add(s);
-        }
-//        final CheckComboBox<String> checkComboBox = new CheckComboBox<String>() ;
-//        checkComboBox.getItems().addAll(roleTypeLabels);
-        RoleListIdView.getItems().addAll(roleTypeLabels);
-
+        //        final CheckComboBox<String> checkComboBox = new CheckComboBox<String>() ;
+        //        checkComboBox.getItems().addAll(roleTypeLabels);
         // Set the items in the CheckComboBox to display the identity type labels
+        // Display the label in the ComboBox
+        // Convert the label back to its corresponding value
+        roleTypeLabels.addAll(roleTypeMap.values());
+        RoleListIdView.getItems().addAll(roleTypeLabels);
         RoleListIdView.setConverter(new StringConverter<>() {
             @Override
             public String toString(String label) {
@@ -241,8 +257,8 @@ public Boolean submit() {
                 RoleEnum type = RoleEnum.typeOf(string);
                 return type == null ? "" : type.name();
             }
-        });
 
+        });
     }
     public ObservableList<CommonIdCodeName> getDataFromDataDepartmentSource(){
         List<CommonIdCodeName> listDepartment = new ArrayList<>();
@@ -258,7 +274,6 @@ public Boolean submit() {
     public Boolean handleOkButton() {
         Boolean result = false;
         // Kiểm tra xem tất cả các trường đã được nhập
-        boolean isEmailValid = false;
         if (isAllFieldsFilled()) {
             // Lấy giá trị từ các thành phần
             String identificationNumber = identificationNumberView.getText();
@@ -272,9 +287,9 @@ public Boolean submit() {
             String email = emailView.getText();
             String identityType = identityTypeView.getConverter().toString(identityTypeLabel);
             String birthDate = birthDateView.getConverter().toString(birthDateRaw);
-            ObservableList<String> roleListLabel  = RoleListIdView.getCheckModel().getCheckedItems();
+            ObservableList<String> roleListLabel = RoleListIdView.getCheckModel().getCheckedItems();
             String role = RoleListIdView.getConverter().toString(String.valueOf(roleListLabel));
-            CommonIdCodeName department = DepartmentIdView.getValue();
+            String department = DepartmentIdView.getValue();
 
             System.out.println("Mã số chứng thức: " + identificationNumber);
             System.out.println("Loaị giấy tờ" + identityType);
@@ -288,85 +303,97 @@ public Boolean submit() {
             System.out.println("Quyền : " + role);
             System.out.println("Phòng ban : " + department);
 
+            // Kiểm tra tính hợp lệ của email
+            boolean isEmailValid = isValidEmail(email);
+            // Kiểm tra tính hợp lệ của số điện thoại
+            boolean isPhoneNumberValid = isValidPhoneNumber(phoneNumber);
 
-
-
-
-//            // Kiểm tra tính hợp lệ của email
-//            boolean isEmailValid = isValidEmail(email);
-//            // Kiểm tra tính hợp lệ của số điện thoại
-//            boolean isPhoneNumberValid = isValidPhoneNumber(phoneNumber);
-
-//            if (isEmailValid && isPhoneNumberValid) {
-////                 Gather the information from the input fields
-////                    UserCreateDTO newUser = UserCreateDTO.builder()
-////                            .identityType(IdentityTypeEnum.typeOf(identityType))
-////                            .identificationNumber(identificationNumber)
-////                            .firstName(firstName)
-////                            .lastName(lastName)
-////                            .code(codeNumber)
-////                            .phoneNumber(phoneNumber)
-////                            .email(email)
-////                            .address(address)
-////                            .birthDate(DateUtils.asDate(birthDateRaw))
-////                            .lstRoleId()
-////                            .build(); }
-//                    // API Call
-//                    try {
-//                        result = patientAPIService.createPatient(newPatient);
-//                    } catch (ApiResponseException exception) {
-//                        exception.printStackTrace();
-//                        System.out.println(exception.getExceptionResponse());
-//                    }
-//                }
-//        } else {
-//            // Hiển thị thông báo lỗi cho các trường không hợp lệ
-//            StringBuilder errorMessages = new StringBuilder();
-//            if (!isEmailValid) {
-//                errorMessages.append("- Email không hợp lệ\n");
-//            }
-//            if (! isValidPhoneNumber(phoneNumber)) {
-//                errorMessages.append("- Số điện thoại không hợp lệ\n");
-//            }
-//            displayErrorAlert(errorMessages.toString());
-//            return false;
-//        }
-    } else {
-            // Hiển thị thông báo lỗi nếu chưa nhập đủ các trường
-            displayErrorAlert("Vui lòng nhập đầy đủ thông tin trước khi tiếp tục.");
+            if (isEmailValid && isPhoneNumberValid) {
+                UserCreateDTO createDTO = UserCreateDTO.builder()
+                        .identificationNumber(identificationNumber)
+                        .identityType(IdentityTypeEnum.typeOf(identityType))
+                        .firstName(firstName)
+                        .lastName(lastName)
+                        .phoneNumber(phoneNumber)
+                        .birthDate(DateUtils.asDate(birthDateRaw))
+                        .email(email)
+                        .departmentId((getIdDepartmentFromName(department)))
+                        .address(address)
+                        .code(codeNumber)
+                        .lstRoleId(getRoleIdFromRoleName(roleListLabel)).build();
+                // API Call
+                try {
+                    result = userAPIService.createUser(createDTO);
+                } catch (ApiResponseException exception) {
+                    exception.printStackTrace();
+                    System.out.println(exception.getExceptionResponse());
+                }
+            }else {
+                StringBuilder errorMessage = new StringBuilder();
+                if (!isEmailValid) {
+                    errorMessage.append("- Email không hợp lệ\n");
+                }
+                if (!isPhoneNumberValid) {
+                    errorMessage.append("- Số điện thoại không hợp lệ\n");
+                }
+                displayErrorAlert(errorMessage.toString());
+                return false;
+            }
+        }else {
+            // Hiển thị thông báo lỗi nếu chưa nhập đủ trường
+            displayErrorAlert("Vui Lòng nhập đầy đủ thông tin trước khi tiếp tục");
             return false;
         }
-        return result;
-    }
-    public Boolean isAllFieldsFilled(){
-        String identificationNumber = identificationNumberView.getText();
-        String identifyType = identityTypeView.getValue();
-        String firstName = firstNameView.getText();
-        String lastName = lastNameView.getText();
-        String code = CodeNumberView.getText() ;
-        LocalDate birthDate = birthDateView.getValue();
-        String address = addressView.getText();
-        String phone = phoneNumberView.getText();
-        String email = emailView.getText();
-        ObservableList<String> roleList = RoleListIdView.getItems();
-        String department = String.valueOf(DepartmentIdView.getValue());
-        return Validation.isAllFieldsFilledAddUser(identificationNumber,firstName,lastName,identifyType,department,roleList,code
-                ,birthDate,address,phone,email);
-    }
-    public boolean isValidEmail(String email) {
-        return Validation.isValidEmail(email);
-    }
-    // validate
-    public boolean isValidPhoneNumber(String phoneNumber) {
-        return Validation.isValidPhoneNumber(phoneNumber);
-    }
-    public void displayErrorAlert(String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Lỗi");
-        alert.setHeaderText("Thông tin không hợp lệ");
-        alert.setContentText(message);
-        alert.showAndWait();
+        return result ;
+        }
+
+        public Boolean isAllFieldsFilled () {
+            String identificationNumber = identificationNumberView.getText();
+            String identifyType = identityTypeView.getValue();
+            String firstName = firstNameView.getText();
+            String lastName = lastNameView.getText();
+            String code = CodeNumberView.getText();
+            LocalDate birthDate = birthDateView.getValue();
+            String address = addressView.getText();
+            String phone = phoneNumberView.getText();
+            String email = emailView.getText();
+            ObservableList<String> roleList = RoleListIdView.getItems();
+            String department = String.valueOf(DepartmentIdView.getValue());
+            return Validation.isAllFieldsFilledAddUser(identificationNumber, firstName, lastName, identifyType, department, roleList, code
+                    , birthDate, address, phone, email);
+        }
+        public boolean isValidEmail (String email){
+            return Validation.isValidEmail(email);
+        }
+        // validate
+        public boolean isValidPhoneNumber (String phoneNumber){
+            return Validation.isValidPhoneNumber(phoneNumber);
+        }
+        public void displayErrorAlert (String message){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Lỗi");
+            alert.setHeaderText("Thông tin không hợp lệ");
+            alert.setContentText(message);
+            alert.showAndWait();
+        }
+        public Long getIdDepartmentFromName (String department){
+            ObservableList<CommonIdCodeName> listDepartment = getDataFromDataDepartmentSource();
+            for (CommonIdCodeName cic : listDepartment) {
+                if (cic.getName().equals(department)) {
+                    return cic.getId();
+                }
+            }
+            return null;
+        }
+        public Set<Long> getRoleIdFromRoleName (ObservableList < String > roleName) {
+            Set<Long> listRoleId = new HashSet<>();
+            for (String roleEnum : roleName) {
+                RoleEnum roleEnum1 = RoleEnum.typeOf(roleEnum);
+                listRoleId.add(roleEnum1.getId());
+            }
+            return listRoleId;
+        }
     }
 
-}
+
 
